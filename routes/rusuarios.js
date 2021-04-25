6ea0f43dc92eab5eac86d5bef9de00236a393443
Alexander <uo266499@uniovi.es>
2021-04-25 18:26:17 +0200

    Ahora al borrar usuarios, tambien se borran las ofertas no compradas de esos usuarios

--- routes/rusuarios.js
+++ routes/rusuarios.js
@@ -221,12 +221,31 @@ module.exports = function(app, swig, gestorBD) {
             }
         }
         let criterioFinal = {"$or": criterio};
-        gestorBD.eliminarUsuarios(criterioFinal, function(usuarios) {
-            if (usuarios == null) {
-                res.send("No hay ningún usuario que borrar");
+        gestorBD.obtenerUsuarios(criterioFinal,function (usuarios) {
+            if (usuarios === null || usuarios.length === 0 ){
+                res.send("Error al recibir usuarios");
             } else {
-                res.redirect('/usuarios');
+                let criterioEmails = [];
+                for (let user of usuarios){
+                    criterioEmails.push({"vendedor" : user.email, "comprador": null });
+                }
+                gestorBD.eliminarOfertas({"$or": criterioEmails},function (ofertas) {
+                    if (ofertas === null || ofertas.length === 0 ){
+                        res.send("Error al eliminar ofertas");
+                    } else {
+                        gestorBD.eliminarUsuarios(criterioFinal, function(usuarios) {
+                            if (usuarios == null) {
+                                res.send("No se ha encontrado el/los usuarios a borrar");
+                            } else {
+                                res.redirect('/usuarios');
+                            }
+                        });
+                    }
+                });
             }
         });
+
+
+
     });
 };
